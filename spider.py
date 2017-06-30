e16158f1d49a819344eed6f830b02ee46985046d
Pengfei.X <pengphy@gmail.com>
2017-06-30 11:26:38 +0800

    stop crawl for every 2 hours

--- spider.py
+++ spider.py
@@ -1,3 +1,4 @@
+import sys
 import time
 
 
@@ -40,10 +41,14 @@ class Spider:
 
 
 if __name__ == '__main__':
+    start = time.time()
     s = Spider()
     s.do_crawl()
     try:
         while True:
+            if time.time() - start > 60 * 60:
+                sys.exit()
+
             time.sleep(2**10)  # actually sleep forever
     except KeyboardInterrupt:
         print('i am gone, Bye!')
